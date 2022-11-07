d1ac069bc1a6a731a55bdeaae21facd4b43858ec
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-07 22:25:40 +0600

    blog question-4

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -9,10 +9,11 @@ const Blog = () => {
       </div>
       
       <div >
-      <div  className='justify-center justify-items-center grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  mt-20 mb-35  gap-4  text-black '>
       <div data-aos="fade-left"
             data-aos-duration="1000"
-            data-aos-delay="200" class="card  card-compact w-96 shadow-xl border-2 border-sky-300">
+            data-aos-delay="200"   className='justify-center justify-items-center grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2  mt-20 mb-35  gap-4  mb-10 text-black  '>
+      <div 
+            class="card  card-compact w-96 shadow-xl border-2 border-sky-300">
   
   <div  class="card-body">
     <h2 class="card-title">Difference between SQL and NoSQL</h2>
@@ -46,6 +47,15 @@ const Blog = () => {
 * Javascript is used to build dynamic HTML pages with interactive effects on a webpage, mobile Application, Games, and Server Application etc. NodeJs facilities JavaScript to be run on a server.<br></br>
 
 * All browsers have in built javascript engines, which is why browsers can run the javascript codes of web pages. Nodejs uses V8 engine which enables it to parse and run javascript code.</p>
+    
+  </div>
+</div>
+<div class="card  card-compact w-96  shadow-xl border-2 border-sky-300">
+  
+  <div class="card-body">
+    <h2 class="card-title">How does NodeJS handle multiple requests at the same time?</h2>
+    <p>NodeJS Web Server maintains a limited Thread Pool to provide services to client requests. Multiple clients make multiple requests to the NodeJS server. NodeJS receives these requests and places them into the EventQueue .
+NodeJS server has an internal component referred to as the EventLoop which is an infinite loop that receives requests and processes them. This EventLoop is single threaded. In other words, EventLoop is the listener for the EventQueue.</p>
     
   </div>
 </div>
