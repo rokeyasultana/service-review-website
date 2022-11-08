b57b8787cf34465f4bf49f7f30d3a988713c31b8
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-08 16:27:12 +0600

    banner

--- src/Pages/Home/Testimonial/Testimonial.js
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -1,16 +1,27 @@
 import React from 'react';
-
+import './Testimonial.css'
 const Testimonial = () => {
     return (
-        <div className='mb-15'>
+        <div id='bg' className='mb-15 grid justify-items-center '>
             
           <div>
             
-        <div className='bg-sky-400 mt-10 mb-80 h-80'>
+        <div className= 'bg-cyan-400   w-4/5 py-2 px-2 mt-28 bg-opacity-40 mb-40'>
 
 <h2 className='mt-5 text-center text-3xl font-semibold text-white '>Best tour service provider,excellent vacation</h2>
 <p  className='mt-5 text-center text-white '>Good things come to those who travel</p>
 <p  className=' text-center  text-white '>& good travel experiences comes to those who get a tour guide.</p>
+
+<div class="avatar flex justify-center items-center mt-5">
+  <div class="w-24 rounded-full ">
+    <img src="https://st2.depositphotos.com/2501951/7446/i/600/depositphotos_74467993-stock-photo-the-tourist-near-the-acropolis.jpg" />
+  </div>
+</div>
+
+<div>
+    <h2 className='text-center  text-xl text-white '>"Alexa Ann"</h2>
+    <p className='text-center text-white'> (Tourist Guide)</p>
+</div>
         </div>
             </div>  
         </div>
