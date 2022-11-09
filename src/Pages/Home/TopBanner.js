2190540f80cb92728e878914e829fd39f9f358dc
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-09 23:14:28 +0600

    top banner

--- src/Pages/Home/TopBanner.js
+++ src/Pages/Home/TopBanner.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import './TopBanner.css'
 const TopBanner = () => {
     return (
         <div>
-            
+            <div id='heroBg' class="hero ">
+  <div class="hero-overlay bg-opacity-30"></div>
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Feel Your life with adventures</h1>
+      <h1 class="mb-5 text-2xl font-bold">Hello I'm Alexa Ann Your travel guide</h1>
+      <p class="mb-5">Travel expands the mind and fills the gap</p>
+     
+    </div>
+  </div>
+</div>
         </div>
     );
 };
